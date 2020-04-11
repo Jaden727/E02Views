793bf5117aa8c99800a43e248e9226fc5598daba
Im, Joohyung <joohyung727@gmail.com>
2020-04-11 20:29:15 +0900

    Mobile_Programming_hw_0411

--- app/src/main/java/net/skhu/MainActivity.java
+++ app/src/main/java/net/skhu/MainActivity.java
@@ -1,7 +1,6 @@
 package net.skhu;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -15,14 +14,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_signup) {
             Intent intent = new Intent(this, SignupActivity.class);
@@ -32,11 +31,14 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this, MemoActivity.class);
             startActivity(intent);
             return true;
-        }
-          else if (id == R.id.action_buttons){
-              Intent intent = new Intent(this, ButtonsActivity.class);
-              startActivity(intent);
-              return true;
+        } else if (id == R.id.action_buttons) {
+            Intent intent = new Intent(this, ButtonsActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.action_checkboxes) {
+            Intent intent = new Intent(this, CheckboxesActivity.class);
+            startActivity(intent);
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
